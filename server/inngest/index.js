80147a1b7ef0df7536b95f358283f7e5d15144e2
VISHAL55UIET <vishalsingh962849@gmail.com>
2026-01-09 13:15:35 +0530

    default

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import UserModel from "../models/User.js";
+import User from "../models/User.js";   // correct model import
 import mongoose from "mongoose";
 import "dotenv/config";
 
@@ -12,35 +12,25 @@ export const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    console.log("User.create event:", event.data);
-
     const emailList = event.data.email_addresses || [];
     const primaryEmail = emailList.length > 0 ? emailList[0].email_address : null;
 
-    if (!primaryEmail) {
-      console.log("No email found, skipping insert");
-      return;
-    }
+    if (!primaryEmail) return;
 
     let username = primaryEmail.split("@")[0];
+    const existing = await User.findOne({ username });
 
-    const existing = await UserModel.findOne({ username });
     if (existing) {
-      username = username + Math.floor(Math.random() * 10000);
+      username += Math.floor(Math.random() * 10000);
     }
 
-    try {
-      await UserModel.create({
-        _id: event.data.id,
-        email: primaryEmail,
-        full_name: event.data.first_name + " " + event.data.last_name,
-        profile_picture: event.data.image_url,
-        username
-      });
-      console.log("User inserted in Mongo");
-    } catch (err) {
-      console.log("Insert failed:", err);
-    }
+    await User.create({
+      _id: event.data.id,
+      email: primaryEmail,
+      full_name: `${event.data.first_name} ${event.data.last_name}`,
+      profile_picture: event.data.image_url,
+      username
+    });
   }
 );
 
@@ -48,23 +38,15 @@ export const syncUserUpdation = inngest.createFunction(
   { id: "update-user-from-clerk" },
   { event: "clerk/user.updated" },
   async ({ event }) => {
-    console.log("User.update event:", event.data);
-
     const emailList = event.data.email_addresses || [];
     const primaryEmail = emailList.length > 0 ? emailList[0].email_address : null;
+    if (!primaryEmail) return;
 
-    if (!primaryEmail) {
-      console.log("No email found, skipping update");
-      return;
-    }
-
-    await UserModel.findByIdAndUpdate(event.data.id, {
+    await User.findByIdAndUpdate(event.data.id, {
       email: primaryEmail,
-      full_name: event.data.first_name + " " + event.data.last_name,
+      full_name: `${event.data.first_name} ${event.data.last_name}`,
       profile_picture: event.data.image_url
     });
-
-    console.log("User updated in Mongo");
   }
 );
 
@@ -72,15 +54,15 @@ export const syncUserDeletion = inngest.createFunction(
   { id: "delete-user-from-clerk" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
-    console.log("User.delete event:", event.data);
-    await UserModel.findByIdAndDelete(event.data.id);
-    console.log("User deleted from Mongo");
+    await User.findByIdAndDelete(event.data.id);
   }
 );
 
 export const functions = [
   syncUserCreation,
   syncUserUpdation,
-  syncUserDeletion
+  syncUserDeletion   // include deletion also
 ];
 
+// ❗MANDATORY EXPORT FOR VERCEL HANDLER
+export default functions;
